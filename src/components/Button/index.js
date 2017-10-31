431e8e06112c8615728d3a7e0e4ceb232bbd019b
Rikin Katyal <me@sirvar.com>
2017-10-31 01:18:33 -0400

    Adjust buttom dimem

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 export default styled.button`
-  width: 225px;
-  height: 45px;
+  width: 250px;
+  height: 50px;
   font-size: 17px;
   background-color: #fd9752;
   color: #ffffff;
@@ -10,5 +10,6 @@ export default styled.button`
   cursor: pointer;
   outline: none;
   font-size: 22px;
+  font-family: Dosis;
   box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.4);
 `;
