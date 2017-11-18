700eea69edaee50974da0a0e4c354e35366cfc57
Rikin Katyal <me@sirvar.com>
2017-11-18 16:47:43 -0500

    Start adding hubspot

--- src/components/ScheduleDemoModal/index.js
+++ src/components/ScheduleDemoModal/index.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import React from "react";
 import Modal from "react-modal";
 import ScrollLock from "react-scrolllock";
+import https from 'https'
+import querystring from 'querystring'
 
 import Colors from "./../../colors";
 
@@ -49,6 +51,64 @@ const SubmitButton = styled.input`
 `;
 
 export default class ScheduleDemoModal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+
+    this.state = {
+      nameValue: '',
+      emailValue: ''
+    };
+  }
+
+  handleChangeEmail(e) {
+    this.setState({
+      emailValue: e.target.value
+    })
+  }
+
+  handleChangeName(e) {
+    this.setState({
+      nameValue: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    var postData = querystring.stringify({
+      'email': this.state.emailValue,
+      'name': this.state.nameValue,
+    });
+    
+    var options = {
+      hostname: 'forms.hubspot.com',
+      path: '/uploads/form/v2/3957039/',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': postData.length
+      }
+    }
+
+    var request = https.request(options, function(response){
+      console.log("Status: " + response.statusCode);
+      console.log("Headers: " + JSON.stringify(response.headers));
+      response.setEncoding('utf8');
+      response.on('data', function(chunk){
+        console.log('Body: ' + chunk)
+      });
+    });
+
+    request.on('error', function(e){
+      console.log("Problem with request " + e.message)
+    });
+
+    request.write(postData);
+    request.end()
+    e.preventDefault();
+  }
+
   render() {
     const { isOpen } = this.props;
     return (
@@ -64,11 +124,11 @@ export default class ScheduleDemoModal extends React.Component {
           <h3>Your Challenges. Our Solutions</h3>
         </center>
         <p>Enter your details and we'll be in touch within a working day.</p>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div>Name:</div>
-          <TextInput type="text" autoFocus required />
+          <TextInput type="text" value={this.state.nameValue} onChange={this.handleChangeName} autoFocus required />
           <div>Email:</div>
-          <TextInput type="email" required />
+          <TextInput type="email" value={this.state.emailValue} onChange={this.handleChangeEmail} required />
           <br />
           <SubmitButton type="submit" value="Submit" />
         </form>
